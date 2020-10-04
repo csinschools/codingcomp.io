ce1e4c934de58b3e3c71cb551275d323d66b7c58
John Sherwood <john.sherwood.was.taken@gmail.com>
2020-10-04 21:41:53 +1100

    A basic content site

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,21 @@
+class PagesController < ApplicationController
+  def main
+
+  end
+
+  def faq
+
+  end
+
+  def about
+
+  end
+
+  def rules
+
+  end
+
+  def entries
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'pages#main'
+  get "/about", to: 'pages#about'
+  get "/rules", to: 'pages#rules'
+  get "/faq", to: 'pages#faq'
+  get "/entries", to: 'pages#entries'
 end
